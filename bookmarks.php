f55349d2f724d429209964542afcc6319ed2806a
Cagliostro <atfield2501@gmail.com>
2018-05-05 15:08:43 +0200

    //Ajout fichier bookmark.php

---
+++ bookmarks.php
@@ -0,0 +1,29 @@
+<?php
+    try
+    {
+         $bdd = new PDO('mysql:host=localhost;dbname=atfield2501;charset=utf8', 'atfield2501', 'mdp::^^');
+    }
+
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
+
+$res = $bdd->query('select count(*) as nb from bookmarks');
+$data = $res->fetch();
+$nb = $data['nb'];
+    $Numero=rand(1 , $nb);
+    $reponssse=$bdd->query("SELECT url FROM bookmarks WHERE id=$Numero");
+
+while ($donnees = $reponssse->fetch())
+{
+
+        $Adresse=''. $donnees['url'].'';
+}
+
+$reponssse->closeCursor();
+ 
+header("Location: $Adresse");
+exit();
+?>
+
