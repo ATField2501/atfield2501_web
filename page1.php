77707545c342be4e5594e2ae0b0a79f76564301d
Cagliostro <atfield2501@gmail.com>
2018-05-06 13:06:32 +0200

    //Ajout fichier menu_1.php et modification page01.php en conséquence (include)

--- page1.php
+++ page1.php
@@ -41,38 +41,8 @@ echo "<em><h6> $citation </h6></em><br >";
 ?>
 
 
+<?php include("menu_1.php"); ?>        
 
-           <div id="conteneur0">
-           <div class="element0"><h4><a href="atfield2501.jpg"><img src="atfield2501_mini.jpg" alt="atfield" title="atfield" /></a></h4></div>
-
-           <div class="element0"><h7> Navigation </h7>
-           <h3> 
-           <a href="Web_irc/chat.php" title="Chat-Box" > Chat-BoX</a>
-           </h3>
-
-           <h3> 
-           <a href="juke-box/jukebox.php" title="Juke-Box" > Juke-BoX</a>
-           </h3>
-
-           <h3> 
-           <a href="espace_perso/formulaire.php" title="Lymbes" > Lymbes</a>
-           </h3>
-
-
-           <h3> 
-           <a href="bookmarks.php" title="BookMark" target="_blank"> BookMark</a>
-           </h3>
-
-
-           <h3> 
-           <a href="ascii-art/ascii_art.php" title="Ascii-Art" > Ascii-Art</a>
-           </h3>
-
-           <h3> 
-           <a href="index.php" title="home" > ::(;,,;)::</a>
-           </h3></div>
-
-           <div class="element0"><hh><img src="shigurui.jpg"/></hh></div></div>           
           
 
        <h5> 
