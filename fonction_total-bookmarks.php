80a366e319f5662569dbcf56370416cb8ab0bc4a
Cagliostro <atfield2501@gmail.com>
2018-05-20 16:29:19 +0200

    //ajout fichier fonction_total-bookmarks et include celle ci dans page01.php

---
+++ fonction_total-bookmarks.php
@@ -0,0 +1,20 @@
+<?php
+
+    try
+    {
+         $bdd = new PDO('mysql:host=localhost;dbname=atfield2501;charset=utf8', 'atfield2501', 'mdp=^^');
+    }
+
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    } 
+
+$res1=$bdd->query('select count(*) as nb1 from bookmarks');             
+
+$data=$res1->fetch();
+$nb=$data['nb1'];
+
+echo "<p> BookMarks : <a>$nb</a> </p>";
+
+?>
