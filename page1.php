dd4ba4375faa744e23dd3ab22addf2d15beb2bf8
Cagliostro <atfield2501@gmail.com>
2018-08-13 12:37:57 +0200

    /ajout image et fichier kanas.html

--- page1.php
+++ page1.php
@@ -48,7 +48,7 @@ include("fonction_identification.php");
        <h5> 
            <?php include("fonction_total-bookmarks.php"); ?>
            <p> irc.epiknet.org #Cthulhu </p>
-           <p>Compte git-hub <a href="https://github.com/ATField2501" target="_blank">ATField2501</a></p>
+           <p>Cloud Dev Git-Hub <a href="https://github.com/ATField2501" target="_blank">ATField2501</a></p>
            <p><a href="mailto:atfield2501@gmail.com">Contacter ATField2501 par e-mail </a></p>
        </h5>
        
