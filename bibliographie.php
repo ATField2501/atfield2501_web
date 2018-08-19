a4c080c459f04f2292cd66445a388d00ba53ad82
Cagliostro <atfield2501@gmail.com>
2018-08-19 14:12:12 +0200

    /modification bibliographie.php avec un include

--- bibliographie.php
+++ bibliographie.php
@@ -48,24 +48,7 @@ echo '<div class="element0">';
 echo "<h3>Total des auteurs : <a>$nb2</a></h3>"; 
 echo '</div>';
 
-echo '<div class="element0">';
-echo '<legend><h4> Recherche</h4></legend>
-';
-echo '<form action="recherche.php" method="post">';
-echo '<h4>';
-
-echo '<input type="texte" name="recherche" />';           
-echo '<input type="submit" value="Valider" /><br />';
-
-echo '<input type="radio" id="recherche" name="recherche"  checked/>';
-echo '<label for="recherche">titre</label>';
-
-echo '<input type="radio" id="recherche" name="recherche"  />';
-echo '<label for="recherche">auteur</label>';
-
-echo '</h4>';
-echo '</form>';
-echo '</div>';
+include("bibleRecherche.php");
 
 ?>
     </body>
