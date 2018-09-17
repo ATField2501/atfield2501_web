9698e60bf95a61fe96c2168d4a8cd91bdcd041da
Cagliostro <atfield2501@gmail.com>
2018-09-17 12:22:59 +0200

    /modification carte_visiteur.php

--- carte_visiteur.php
+++ carte_visiteur.php
@@ -1,6 +1,13 @@
-       <h5> 
+ 	
+    <h5>
+        <div2 id="conteneuralpha">
            <?php include("fonction_total-bookmarks.php"); ?>
-           <p> IRC epiknet.org #Cthulhu </p>
+           <p>IRC epiknet.org #Cthulhu</p> 
            <p>Cloud Dev Git-Hub <a href="https://github.com/ATField2501" target="_blank">ATField2501</a></p>
-           <p><a href="mailto:atfield2501@gmail.com">Contacter ATField2501 par e-mail </a></p>
-       </h5>
+           <p><a href="mailto:atfield2501@gmail.com">Contacter ATField2501 par e-mail </a></p></div2>
+
+    </h5>
+
+       
+
+
