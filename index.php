fb108775d40e34ca1ee0aec4dcd44dfa2bb03871
Cagliostro <atfield2501@gmail.com>
2018-09-22 12:08:19 +0200

    // ^(;,,;)^

--- index.php
+++ index.php
@@ -13,25 +13,16 @@
         </div> 
 <?php
 
-$monfichier = fopen('compteur.txt', 'r+');
+    $monfichier = fopen('compteur.txt', 'r+');
+    $pages_vues = fgets($monfichier); // On lit la première ligne (nombre de pages vues)
+    $pages_vues += 1; // On augmente de 1 ce nombre de pages vues
+    fseek($monfichier, 0); // On remet le curseur au début du fichier
+    fputs($monfichier, $pages_vues); // On écrit le nouveau nombre de pages vues
+    fclose($monfichier);
 
-$pages_vues = fgets($monfichier); // On lit la première ligne (nombre de pages vues)
-
-$pages_vues += 1; // On augmente de 1 ce nombre de pages vues
-
-fseek($monfichier, 0); // On remet le curseur au début du fichier
-
-fputs($monfichier, $pages_vues); // On écrit le nouveau nombre de pages vues
-
-fclose($monfichier);
-
-echo '<h2>total   ' . $pages_vues . ' vues</h2>';
-
-
-
-echo ' <h2>'. date("d-m-Y") .' </h2>';
-include("fonction_identification.php"); 
-
+    echo '<h2>total   ' . $pages_vues . ' vues</h2>';
+    echo ' <h2>'. date("d-m-Y") .' </h2>';
+    include("fonction_identification.php"); 
 ?>
     </body>
 </html>
