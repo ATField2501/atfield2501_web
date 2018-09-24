f14e03e1ed8243bbb91490a864f89673cbbba5f0
Cagliostro <atfield2501@gmail.com>
2018-09-24 15:38:39 +0200

    /script fonction_identification.php fonctionnel

--- fonction_identification.php
+++ fonction_identification.php
@@ -8,7 +8,7 @@ $Heure=date("H:i");
 try
 {
 
-       $bdd = new PDO('mysql:host=localhost;dbname=atfield2501;charset=utf8', 'atfield2501', 'mdp==^^');
+       $bdd = new PDO('mysql:host=localhost;dbname=atfield2501;charset=utf8', 'atfield2501', 'mdp::^^');
 }  
   
 catch(Exception $e)
@@ -18,21 +18,31 @@ catch(Exception $e)
 
 }
 
-// Test de l'esxistence de l'IP du visiteur dans la table sql
-$reponse = $bdd->query('SELECT * FROM Visiteurs WHERE ip_adresse=$ip_adresse');
 
-// Si réponse négative, enregistrement complet
-if ($reponse)
+$res = $bdd->query("SELECT COUNT(*) AS nb FROM Visiteurs WHERE ip_adresse='$ip_adresse'");
+$data = $res->fetch();
+$nb = $data['nb'];
+
+if($nb == 0) 
 {
-       $bdd->exec("UPDATE Visiteurs SET date=\"$Date\" , heure=\"$Heure\",user_agent=\"$User_agent\" WHERE ip_adresse=\"$ip_adresse\"") or die(print_r($bdd->errorInfo()));
+
+        $req = $bdd->prepare('INSERT INTO Visiteurs (ip_adresse, nb, date, heure, user_agent) VALUES(?,?,?,?,?)');
+        $req->execute(array($_POST['ip_adresse'] = $ip_adresse , $_POST['nb'] = 1 , $_POST['date'] = date("d-m-Y"), $_POST['heure'] = date("H:i"), $_POST['user_agent'] =$User_agent));
 }
 
-// Sinon mise à jour des infos liées au champ ip_adresse
-else 
+else
 {
-       $req = $bdd->prepare('INSERT INTO Visiteurs (ip_adresse, date, heure, user_agent) VALUES(?,?,?,?)');
-
-       $req->execute(array($_POST['ip_adresse'] = $ip_adresse , $_POST['date'] = date("d-m-Y"), $_POST['heure'] = date("H:i"), $_POST['user_agent'] =$User_agent));       
- 
+        $bdd->exec("UPDATE Visiteurs SET nb=nb+1, date=\"$Date\" , heure=\"$Heure\",user_agent=\"$User_agent\" WHERE ip_adresse=\"$ip_adresse\"") or die(print_r($bdd->errorInfo()));  
 }
+
+
+
+
+
 ?>
+
+
+
+
+
+
