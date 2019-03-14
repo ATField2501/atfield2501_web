103c421f0ce7f64282bf8bf5bb9a3cbbdc6b6304
Cagliostro <atfield2501@gmail.com>
2019-03-14 10:56:23 +0100

    /Modification de Filtre.php pour qu'il rende compte de son activité

--- Filtre.php
+++ Filtre.php
@@ -6,9 +6,16 @@
         while (!feof($blacklist)) ## tans que le fichier n'est pas entièrement lu
         {
             $ligne = fgets($blacklist);
-            # trim() pour effacer charactères invisibles
+
             if (trim($ligne) == $ip_adresse)
             {
+                # Renseignement sur l'activitée du firewall
+                $monfichier = fopen('../espace_perso/firewall_activite.txt', 'r+');
+                $pages_vues = fgets($monfichier); // On lit la première ligne (nombre de pages vues)
+                $pages_vues += 1; // On augmente de 1 ce nombre de pages vues
+                fseek($monfichier, 0); // On remet le curseur au début du fichier
+                fputs($monfichier, $pages_vues); // On écrit le nouveau nombre de pages vues
+                fclose($monfichier);
                 header('Location: ../index.php');
             }
         }
