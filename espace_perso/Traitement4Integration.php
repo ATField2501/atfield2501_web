a06d0008ff8d495339555ac0224737b8cb640f1a
Cagliostro <atfield2501@gmail.com>
2019-03-14 17:43:27 +0100

    /ajout Admin_jukeBox.php et Traitement4Integration.php

---
+++ espace_perso/Traitement4Integration.php
@@ -0,0 +1,44 @@
+<?php
+    # Récupération des valeurs du formulaire
+    $style = $_POST["select"];
+    $nazca = $_POST["nazca"];
+
+    # Split du code entourant la cible
+    $fruit= explode('src="' , $nazca , 2);
+    $objet= $fruit[1];
+    $fruit1= explode('"' , $objet);
+    $url= $fruit1[0];
+
+
+    # Connexxion à la bdd
+    try
+    {
+         $bdd = new PDO('mysql:host=localhost;dbname=atfield2501;charset=utf8', 'atfield2501', 'mdp=^^');
+    }
+    catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    } 
+
+    # Vérification d'une présence éventuelle de la valeur dans la bdd
+#    $ether = $bdd->query("select url from hiphop");
+#    while ($data = $ether->fetch())
+#    {
+#        $base = $data['url'];
+#        # Si l valeur n'est pas déja présente dans la bdd 
+#        if ($data != $url)
+#        {
+#            # Enregistrement de la valeur dans la bdd 
+#            $bdd->exec("INSERT INTO $style (url) VALUES($dozo)");
+#        } 
+#    }	
+    
+    
+            # Enregistrement de la valeur dans la bdd 
+            $bdd->exec("INSERT INTO $style (url) VALUES('$url')");
+
+
+    header('Location: Admin_JukeBox.php');
+
+?>
+
