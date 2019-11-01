9039231723aafb9b070da49dd7a8496364ac0c49
ATField2501 <atfield2501@gmail.com>
2019-11-01 17:50:24 +0100

    qrcode page donnation + Dossier QRcodes

--- donnation.php
+++ donnation.php
@@ -8,8 +8,13 @@
 <h2> Si vous avez l'envis de me soutenir dans l'entretien de ce site artisanal et open-source (html/css/(php/python))@gnu/linux (;,,;) </h2>
 <br><br>
 <h1> adresse bitcoin: bc1qkd0uk02v6a3nc7q5z8xnndnsnflr9rgyvapd6y</h1>
-<br>
+<h1><img src="QRcodes/Caglio-Bitcoin-Wallet.png" alt="Caglio-Bitcoin-Wallet.png"/><br> Bitcoin</h1>
+<br><br>
 <h1> adresse ethereum: 0x7C2C125EDb3AaF591d0Fcd22b3605b26E14B3e04 </h1>
+<h1><img src="QRcodes/Caglio-Ethereum-Wallet.png" alt="Caglio-Ethereum-Wallet.png"/><br> Ethereum</h1>
+<br>
+<h1> adresse Ark: Aa6jQSDJaqx9MHkHWiff6wJNKV3vwLyE5y </h1>
+<h1><img src="QRcodes/Caglio-Ark-Wallet.png" alt="Caglio-Ark-Wallet.png"/><br> Ark</h1>
 <br>
 <h1> adresse bitcoincore: pas encore...</h1>
 <br><br><br><br>
